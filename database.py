276058dfc6791b9e357fd8ab7fdca447a2adb08b
LenaGyebi <abigailgyebi@gmail.com>
2024-01-30 07:21:21 -0800

    user inputs into db

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, MetaData
+from sqlalchemy import create_engine,Table, Column, Integer, String, DateTime, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,13 +9,15 @@ metadata_obj = MetaData()
 Base = declarative_base()
 
 
-class User(Base):
-    __tablename__ = 'users', metadata_obj, 
+user = Table(
+    'users', metadata_obj, 
 
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
-    password = Column(String(50), unique=True, nullable=False)
+    Column("id", Integer, primary_key=True),
+    Column("user_name", String(50), unique=True, nullable=False),
+    Column("email", String(50), unique=True, nullable=False),
+    Column("password", String(50), unique=True, nullable=False)
+    )
+
 
 
 for t in metadata_obj.sorted_tables:
--- main.py
+++ main.py
@@ -1,33 +1,74 @@
+import psycopg2
+from psycopg2 import Error
 from fastapi import FastAPI
-
+import database
 import getpass
 
+#Creating connection to the database
+try:
+    connection = psycopg2.connect(user="postgres",
+                              password="admin",
+                              host="127.0.0.1",
+                              port="5432",
+                              database="userauthen")
 
-app = FastAPI()
+    cursor=connection.cursor()
+    print("PostgresSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    cursor.execute("SELECT version()")
+    record = cursor.fetchone()
+    print("You are connected to -", record, "\n")
+
+
+    app = FastAPI()
+
+
+    user_name = input("Please enter your username: ")
+    email = input("enter your email: ")
+    password = getpass.getpass("Enter your password: ")
+
+except(Exception, Error) as Error:
+    print("Eroor while connecting to database", Error)
 
 
 
-database = {'abena': '2345', 'Lena':'iwantto', 'Adlai':'ishetheone'}
 
-# username = input("Please enter your username: ")
-# password = getpass.getpass("Enter your password: ")
+#inserting the values into the database
+    cursor = connection.cursor()
+    insert_query = """INSERT INTO userauthen (id, user_name,email, password) VALUES (%s, %s, %s,%s)""", 
+    (id, user_name, email, getpass)
+    cursor.execute(insert_query)
+    connection.commit()
+    print("Values have been added to the table successfully")
 
-# for i in database.keys():
+    cursor.execute("SELECT * from userauthen")
+    record = cursor.fetchall()
+    print("Result: ", record)
+
+
+
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("Database has been closed")
+                                                                                              
+# for i in database():
 #         if username == i:
 #             if password != database.get(i):
 #                 password = getpass.getpass("Please enter the password again: ")
 #                 break 
 #             print("Varified! Access granted")
-            # while password != database.get(i):
-            #     password = getpass.getpass("Please re-enter password: ")
-            #     break
-            # print("Verified! Access Granted!")
-            # if password == j:
-            #     print("Verified! Access granted")
-            # else:
-            #     print("Access Denied")
+#             while password != database.get(i):
+#                 password = getpass.getpass("Please re-enter password: ")
+#                 break
+#             print("Verified! Access Granted!")
+#             if password == j:
+#                 print("Verified! Access granted")
+#             else:
+#                 print("Access Denied")
 
 
-@app.get("/root")
-async def root():
-    return("this is first data")
+# @app.get("/root")
+# async def root():
+#     return("this is first data")
