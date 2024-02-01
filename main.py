78c1cccb9d4422e217b50cc0bf1d7c0736972c12
LenaGyebi <abigailgyebi@gmail.com>
2024-02-01 14:12:16 -0800

    API and Database included

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import psycopg2
 from psycopg2 import Error
 from fastapi import FastAPI
-import getpass
-
 
 app = FastAPI()
 
@@ -28,12 +26,6 @@ try:
     connection.commit()
     print("table successfully created")
     
-    # print("PostgresSQL server information")
-    # print(connection.get_dsn_parameters(), "\n")
-    # cursor.execute("SELECT version()")
-    # record = cursor.fetchone()
-    # print("You are connected to -", record, "\n")
-
 
 except(Exception, Error) as Error:
     print("Error while connecting to database", Error)
@@ -43,8 +35,7 @@ except(Exception, Error) as Error:
 def read_root():
     return{"message:" "Welcome to register API"}
 
-    #inserting the values into the database
-    # cursor = connection.cursor()
+    
 @app.post("/register/")
 async def register_user(id: int, lastname: str, firstname: str, user_name: str, email: str, password:str):
     insert_query = """INSERT INTO users(id, lastname, firstname, user_name,email, password) VALUES (%s, %s,%s, %s, %s,%s)"""
@@ -57,10 +48,7 @@ async def register_user(id: int, lastname: str, firstname: str, user_name: str, 
         connection.commit()
         return {"message": "Registration successful"}
 
-        
-
-        # return {"message": "Registration successful"}
-    
+            
     except psycopg2.Error as e:
         return{"Error:", e}
 
@@ -73,7 +61,7 @@ async def register_user(id: int, lastname: str, firstname: str, user_name: str, 
 def read_users():
     cursor.execute("SELECT * from users")
     record = cursor.fetchall()
-    return{"Result": record}
+    return{"Result ": record}
 
 
 
